f57d0a2369fae897a701bbff046e926ae22cdf66
xga <xavier.garceau-aranda@owasp.org>
2020-09-16 13:24:29 +0200

    Improve web app

--- includes/config.php
+++ includes/config.php
@@ -8,17 +8,17 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'SCC Inc.',
+        'name' => 'SCC 成立公司',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
-            '' => 'Home',
-            'products' => 'Products',
-            'about-us' => 'About Us',
+            '' => '家',
+            'products' => '产品展示',
+            'about-us' => '关于我们',
         ],
         'template_path' => 'template',
         'content_path' => 'content',
-        'version' => 'v0.1',
+        'version' => '版 0.1',
     ];
 
     return isset($config[$key]) ? $config[$key] : null;
